41602c7eeef7f6d59253730dee8619ca6364c2e8
Kyros <kevin22fr@gmail.com>
2024-03-27 22:36:36 -0400

    regler bulletmon erreur mb

--- Assets/Scripts/ShootingController.cs
+++ Assets/Scripts/ShootingController.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ShootingController : MonoBehaviour
 {
+    public GameObject Bullet;
     public GameObject Laser;
     public GameObject Cannon;
+    public GameObject BulletsParent;
+    public float BulletSpeed = 1.0f;
+    public float ShootingRate = 1.0f;
     public GameObject LaserParent;
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            GameObject bulletCopy = Instantiate(Bullet);
+            bulletCopy.transform.parent = BulletsParent.transform;
+            bulletCopy.transform.position = Cannon.transform.position;
+            // TODO: ne pas avoir "(transform.eulerAngles.y == 180 ? -1 : 1)" et shooter selon la direction pointe
+            bulletCopy.GetComponent<Rigidbody2D>().AddForce(new Vector2((transform.eulerAngles.y == 180 ? -1 : 1) * BulletSpeed, 0));
             RaycastHit2D hit = Physics2D.Raycast(new(Cannon.transform.position.x, Cannon.transform.position.y), Mathf.Abs(transform.rotation.y) == 1 ? Vector2.left : Vector2.right);
 
             if (hit.collider != null && hit.collider.tag == "Enemie")
@@ -28,7 +35,7 @@ public class ShootingController : MonoBehaviour
                 copieLaser.GetComponent<LineRenderer>().SetPosition(1, hit.collider.transform.position);
 
                 copieLaser.GetComponent<LineRenderer>().enabled = true;
-                
+
                 Destroy(hit.collider.gameObject);
             }
         }
